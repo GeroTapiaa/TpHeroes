e83f923b6ce30edd4dd550dad3bbd15249d2e598
GeroTapiaa <gerook98@hotmail.com>
2022-06-24 14:21:04 -0300

    agregar rutas

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+const path = require("path")
 const port = 3030;
 
-app.get()
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "views", "index.html")))
+app.get("/babbage", (req, res) => res.sendFile(path.join(__dirname, "views", "babbage")))
+app.get("/berners-lee", (req, res) => res.sendFile(path.join(__dirname, "views", "berners-lee")))
+app.get("/clarke", (req, res) => res.sendFile(path.join(__dirname, "views", "clarke")))
+app.get("/hamilton", (req, res) => res.sendFile(path.join(__dirname, "views", "hamilton")))
+app.get("/hopper", (req, res) => res.sendFile(path.join(__dirname, "views", "hopper")))
+app.get("/lovelace", (req, res) => res.sendFile(path.join(__dirname, "views", "lovelace")))
+app.get("/turing", (req, res) => res.sendFile(path.join(__dirname, "views", "turing")))
 
 app.listen(port , () => console.log("Server running in htpp://localhost:" + port));
